51f4b563ba338c4fc65f16e65870e45b3ec8ff59
FriskIsGit <frisk00vexor@wp.pl>
2022-07-13 02:27:42 +0200

    added queries, monad-like generation

---
+++ Shitcord/Services/Queries/DeleteQuery.cs
@@ -0,0 +1,56 @@
+namespace Shitcord.Services.Database;
+
+public class DeleteQuery
+{
+    private string table;
+    private string c1;
+    private string oper;
+    private object val;
+
+    //DELETE FROM t WHERE c1 = val;
+    
+    public DeleteQuery From(string tableName)
+    {
+        table = tableName;
+        return this;
+    }
+    public DeleteQuery Where(string columnName)
+    {
+        c1 = columnName;
+        return this;
+    }
+    //hides the method from the object class
+    public new DeleteQuery Equals(object value)
+    {
+        oper = "=";
+        val = value;
+        return this;
+    }
+    public DeleteQuery IsLess(object value)
+    {
+        oper = "<";
+        val = value;
+        return this;
+    }
+    public DeleteQuery IsMore(object value)
+    {
+        oper = ">";
+        val = value;
+        return this;
+    }
+    
+    public string Build()
+    {
+        if (table == null)
+        {
+            throw new Exception("A required field is null");
+        }
+        if (c1==null || oper==null || val==null)
+        {
+            //deletes all rows from table
+            return $"DELETE FROM {table}";
+        }
+        return $"DELETE FROM {table} WHERE {c1} {oper} {val};";
+    }
+
+}
---
+++ Shitcord/Services/Queries/InsertQuery.cs
@@ -0,0 +1,102 @@
+using System.Text;
+
+namespace Shitcord.Services.Database;
+
+public class InsertQuery
+{
+    private string table;
+    private string[] cols;
+    private object[] vals;
+
+    //INSERT INTO t (c1, c2, c3, ...) VALUES (x1, x2, x3, ...);
+    
+    public InsertQuery Into(string tableName)
+    {
+        table = tableName;
+        return this;
+    }
+    
+    //not necessary to call columns as long as the order of values is known
+    public InsertQuery Columns(params string[] columnNames)
+    {
+        if (columnNames.Length == 0)
+        {
+            return this;
+        }
+        cols = columnNames;
+        return this;
+    }
+    
+    public InsertQuery Values(params object[] values)
+    {
+        if (values.Length == 0)
+        {
+            return this;
+        }
+        vals = values;
+        return this;
+    }
+    
+    public string Build()
+    {
+        if (table==null || vals==null)
+        {
+            throw new Exception("A required field is null");
+        }
+        StringBuilder queryBuilder = new StringBuilder($"INSERT INTO {table} ");
+        if (cols != null)
+        {
+            if (cols.Length != vals.Length)
+            {
+                throw new Exception("Number of column parameters differed from the number of values");
+            }
+            queryBuilder.Append('(');
+            AppendColumnNames(queryBuilder, cols);
+            queryBuilder.Append(") ");
+        }
+
+        queryBuilder.Append("VALUES (");
+        AppendValues(queryBuilder, vals);
+        queryBuilder.Append(')');
+        return queryBuilder.ToString();
+    }
+
+    private void AppendValues(StringBuilder sb, object[] values)
+    {
+        for (int i = 0; ; i++)
+        {
+            if (i == values.Length - 1)
+            {
+                if (values[i] is string)
+                {
+                    sb.Append('"');
+                    sb.Append(values[i]);
+                    sb.Append('"');
+                    break;
+                }
+                sb.Append(values[i]);
+                break;
+            }
+            if (values[i] is string)
+            {
+                sb.Append('"');
+                sb.Append(values[i]);
+                sb.Append("\",");
+                continue;
+            }
+            sb.Append(values[i]).Append(',');
+        }
+    }
+    private void AppendColumnNames(StringBuilder sb, string[] cols)
+    {
+        for (int i = 0; ; i++)
+        {
+            if (i == cols.Length - 1)
+            {
+                sb.Append(cols[i]);
+                break;
+            }
+            sb.Append(cols[i]).Append(',');
+        }
+    }
+}
