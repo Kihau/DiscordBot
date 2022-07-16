26746e5c2385e3e43dc2d478a5031456d0dda674
Kihau <michal.haldas@gmail.com>
2022-07-16 18:49:06 +0200

    Update

--- Shitcord/Data/GuildMarkovData.cs
+++ Shitcord/Data/GuildMarkovData.cs
@@ -2,14 +2,19 @@ using DSharpPlus;
 using DSharpPlus.Entities;
 using System.Collections.Concurrent;
 using Shitcord.Services;
+using Shitcord.Database;
+using Shitcord.Database.Queries;
 
 namespace Shitcord.Data;
 
 public class GuildMarkovData
 {
+    public DiscordGuild Guild { get; }
+    public DatabaseService Database { get; } 
+
     public bool IsEnabled { get; set; } = true;
     // NOTE: Adding channel blocks either auto response and data gathering
-    public List<ulong> ExcludedChannelIDs { get; set; } = new();
+    public List<ulong>? ExcludedChannelIDs { get; set; } = new();
 
     public const int MAX_CHANCE = 1000;
     public bool ResponseEnabled { get; set; } = false;
@@ -21,4 +26,27 @@ public class GuildMarkovData
     public const int DEFAULT_MAX = 28;
     public int MinChainLength { get; set; } = 8;
     public int MaxChainLength { get; set; } = 28;
+
+    public GuildMarkovData() {}
+    public GuildMarkovData(DiscordGuild guild, DatabaseService database) 
+    {
+        Guild = guild;
+        Database = database;
+        LoadDataFromDatabase();
+    }
+
+    public void LoadDataFromDatabase()
+    {
+        var data = Database.GatherData(QueryBuilder
+            .New().Retrieve("*").From(GuildMarkovTable.TABLE_NAME)
+            .WhereEquals(GuildMarkovTable.GUILD_ID, Guild.Id).Build()
+        );
+
+        ExcludedChannelIDs = Database.GatherData(QueryBuilder
+            .New().Retrieve(MarkovExcludedChannelsTable.EXCLUDED_ID)
+            .From(MarkovExcludedChannelsTable.TABLE_NAME)
+            .WhereEquals(MarkovExcludedChannelsTable.GUILD_ID, Guild.Id)
+            .Build()
+        )?.Select(x => (ulong)(long)x[0]).ToList();
+    }
 }
