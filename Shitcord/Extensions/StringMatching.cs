fabde4b7f2b765a38bec83f9d3e0752e70ddbc63
FriskIsGit <friskisgit@users.noreply.github.com>
2023-06-08 22:37:39 +0200

    added string matching extension

---
+++ Shitcord/Extensions/StringMatching.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Shitcord.Extensions;
+
+public class StringMatching{
+    private const int MIN_LEN_THRESHOLD = 2;
+    // larger value = better matching
+    public static int Accuracy(string name, string target){
+        if(name.Length == 0 || target.Length == 0)
+            return 0;
+
+        int accuracy = 0;
+        string[] names = name.ToLower().Split(" ");
+        string[] targets = target.ToLower().Split(" ");
+        foreach (string q in names){
+            foreach (string t in targets){
+                int tmp = matchingLen(q, t);
+                if (tmp >= MIN_LEN_THRESHOLD){
+                    accuracy += tmp;
+                }
+            }
+        }
+        //int effectiveLength = names.Sum(q => q.Length);
+        return Math.Min(accuracy, target.Length);
+    }
+
+    public static int matchingLen(string str1, string str2){
+        int score = 0;
+        int minLen = Math.Min(str1.Length, str2.Length);
+        for (int i = 0, j = 0; i < minLen && j < minLen; i++, j++){
+            char chr1 = str1[i];
+            char chr2 = str2[j];
+            if(chr1 == chr2){
+                score++;
+            }else{
+                if(j+1 < str2.Length && chr1 == str2[j+1]){
+                    // cot -> coat (missed letter)
+                    score++;
+                    j++;
+                }
+                else if(i+1 < str1.Length && str1[i+1] == chr2){
+                    // coat -> cot (additional letter)
+                    score++;
+                    i++;
+                }
+            }
+            //coat -> cost
+        }
+        return score;
+    }
+}
---
+++ Shitcord/Tests/MatchingTest.cs
@@ -0,0 +1,33 @@
+﻿using Shitcord.Extensions;
+
+namespace Shitcord.Tests;
+
+public class MatchingTest{
+    private static int passedTests, failedTests;
+    
+    public static void RunTests(){
+        test("dekstop", "desktop", 6);
+        test("beeee gees", "beeee", 5);
+        test("gees", "skeler", 2);
+        test("lyrics", "lyics", 4);
+        test("coat", "cost", 3);
+        test("post", "post", 4);
+        test("cot", "coat", 3);
+        test("coat", "cot", 3);
+        test("bee gees", "skeler", 2);
+        test("bee gees", "beeeez G", 5);
+        test("recur", "owner", 0);
+        test("q", "something", 0);
+        Console.WriteLine($"Passed/All  {passedTests}/{passedTests+failedTests}");
+    }
+    private static void test(string query, string target, int expected){
+        int accuracy = StringMatching.Accuracy(query, target);
+        if (accuracy == expected){
+            passedTests++;
+        }else{
+            failedTests++;
+            Console.WriteLine($"Expected: {expected}, Got:{accuracy}");
+            Console.WriteLine($"For: {query} | {target}");
+        }
+    }
+}
